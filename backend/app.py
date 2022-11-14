258712f1f78e0d900e8770d154152d608b49e088
annkonna <47333987+annkonna@users.noreply.github.com>
2022-11-13 21:50:18 -0500

    Initial attempt for api endpoint for the topic filter selection

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, jsonify, request,send_file
 import urllib
-from config import URL_solr,CORE_NAME
+# from config import URL_solr,CORE_NAME
 import json
+from configparser import ConfigParser
+import ast
+  
+configur = ConfigParser()
+configur.read('config.ini')
+# print ("Installation Library : ", ast.literal_eval(configur.get('tools','topic_dict')))
+curr_topic_dict = ast.literal_eval(configur.get('tools','topic_dict'))
 
 app = Flask(__name__)
 @app.route('/query', methods = ['GET'])
@@ -26,6 +33,15 @@ def query():
         "response": docs[0]['body'],
     }
     return jsonify(response)
+
+
+@app.route('/query', methods = ['POST'])
+def topic_picked(topic_dict):
+    js=request.get_json()
+    topic = str(js['text'])
+    topic_dict.update({topic: False})
+    return topic_dict
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9999)
