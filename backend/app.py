bb20cc1df5c9ca4deee38e3474f2ea606881e408
annkonna <47333987+annkonna@users.noreply.github.com>
2022-11-14 21:44:46 -0500

    Added logic to put updated topic dict in file

--- backend/app.py
+++ backend/app.py
@@ -7,9 +7,34 @@ import ast
 configur = ConfigParser()
 configur.read('config.ini')
 # print ("Installation Library : ", ast.literal_eval(configur.get('tools','topic_dict')))
-topic_dict = ast.literal_eval(configur.get('tools','topic_dict'))
-URL_solr = ast.literal_eval(configur.get('tools','URL_solr'))
-CORE_NAME = ast.literal_eval(configur.get('tools','CORE_NAME'))
+topic_dict = ast.literal_eval(configur.get('topic dictionary','topic_dict'))
+print(topic_dict)
+topic_dict['All'] = not topic_dict['All']
+print(topic_dict)
+URL_solr = configur.get('tools','URL_solr')
+print(URL_solr)
+CORE_NAME = configur.get('tools','CORE_NAME')
+print(CORE_NAME)
+print(not ast.literal_eval(configur['topic dictionary']["topic_dict"])["All"])
+# configur.write('topic dictionary', topic_dict)
+configur.remove_section('topic dictionary')
+print(configur.sections())
+configur.add_section('topic dictionary')
+configur.set("topic dictionary", "topic_dict", str(topic_dict))
+with open("config.ini", 'w') as configfile:
+        configur.write(configfile)
+
+
+def write_to_file(file_name):
+    with open(file_name, 'w') as configfile:
+        configur.write(configfile)
+
+
+def remove_readd_section(section, topic_dictionary):
+    configur.remove_section('topic dictionary')
+    configur.add_section('topic dictionary')
+    configur.set("topic dictionary", "topic_dict", str(topic_dictionary))
+
 
 app = Flask(__name__)
 @app.route('/query', methods = ['GET'])
