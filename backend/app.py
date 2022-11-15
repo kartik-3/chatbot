c01e7fdeb5aba432f67db3044936e6baca82ca9d
Siddhant <gsiddhant159@gmail.com>
2022-11-15 16:07:22 -0500

    swithced to YAML config; added topic 'All'

--- backend/app.py
+++ backend/app.py
@@ -1,44 +1,18 @@
-from flask import Flask, jsonify, request,send_file
+from flask import Flask, jsonify, request
 import urllib
 import json
-from configparser import ConfigParser
-import ast
-  
-configur = ConfigParser()
-configur.read('config.ini')
-# print ("Installation Library : ", ast.literal_eval(configur.get('tools','topic_dict')))
-topic_dict = ast.literal_eval(configur.get('topic dictionary','topic_dict'))
-print(topic_dict)
-topic_dict['All'] = not topic_dict['All']
-print(topic_dict)
-URL_solr = configur.get('tools','URL_solr')
-print(URL_solr)
-CORE_NAME = configur.get('tools','CORE_NAME')
-print(CORE_NAME)
-print(not ast.literal_eval(configur['topic dictionary']["topic_dict"])["All"])
-# configur.write('topic dictionary', topic_dict)
-configur.remove_section('topic dictionary')
-print(configur.sections())
-configur.add_section('topic dictionary')
-configur.set("topic dictionary", "topic_dict", str(topic_dict))
-with open("config.ini", 'w') as configfile:
-        configur.write(configfile)
-
-
-def write_to_file(file_name):
-    with open(file_name, 'w') as configfile:
-        configur.write(configfile)
-
-
-def remove_readd_section(section, topic_dictionary):
-    configur.remove_section('topic dictionary')
-    configur.add_section('topic dictionary')
-    configur.set("topic dictionary", "topic_dict", str(topic_dictionary))
-
+import config
 
 app = Flask(__name__)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9999)
+
+
 @app.route('/query', methods = ['GET'])
 def query():
+    URL_solr = config.read()['URL_solr']
+    CORE_NAME = config.read()['CORE_NAME']
+    topics = config.read()['topics']
     js=request.get_json()
     qtext = str(js['text'])
     query_params = [
@@ -61,13 +35,10 @@ def query():
     return jsonify(response)
 
 
-@app.route('/query', methods = ['POST'])
-def topic_toggle():
+@app.route('/filter_toggle', methods = ['POST'])
+def toggle_topics():
     js=request.get_json()
     topic = str(js['text'])
-    topic_dict[topic] = not topic_dict[topic]
-    return jsonify(topic_dict)
+    return jsonify(config.toggle(topic))
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9999)
---
+++ backend/config.py
@@ -0,0 +1,21 @@
+import yaml 
+path = 'config.yaml'
+def read():
+    with open(path,'r') as fp:
+        cfg = yaml.safe_load(fp)
+    return cfg
+
+def write(cfg):
+    with open(path,'w') as fp:
+        yaml.dump(cfg,fp)
+
+def toggle(topic):
+    cfg = read()
+    status = cfg['topics'][topic]
+    if topic == 'All':
+        for k,v in cfg['topics'].items():
+            cfg['topics'][k] = not v
+    else:
+        cfg['topics'][topic] = not status
+    write(cfg)
+    return status
