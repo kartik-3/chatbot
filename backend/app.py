2ac16571d67844a733022928472ae09c4a3cd7f3
Siddhant <gsiddhant159@gmail.com>
2022-11-25 21:43:23 -0500

    removed topic filter temporarily

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,6 @@ import json
 import config
 
 app = Flask(__name__)
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9999)
-
-
 @app.route('/query', methods = ['GET'])
 def query():
     URL_solr = config.read()['URL_solr']
@@ -21,20 +17,19 @@ def query():
         'fl=*,score',
         'rows=20',
         'qf=parent_body',
-        f"fq=topic:{' '.join(_get_topics())}"
+        # f"fq=topic:{' '.join(config.get_topics())}"
     ]
     qtext = urllib.parse.quote(qtext)
     query = "&".join([qtext]+query_params)
     query = f'{URL_solr}{CORE_NAME}/query?q={query}'
 
-    resp = urllib.request.urlopen(query)
-    docs = json.load(resp)['response']['docs']
+    page = urllib.request.urlopen(query)
+    docs = json.load(page)['response']['docs']
 
     response = {
-        "response": docs[0]['body'],
+        "response": docs[0]['body'] if docs else "No results found"
     }
     return jsonify(response)
-
 
 @app.route('/filter_toggle', methods = ['POST'])
 def toggle_topics():
@@ -42,6 +37,5 @@ def toggle_topics():
     topic = str(js['text'])
     return jsonify(config.toggle(topic))
 
-def _get_topics():
-    return [topic for topic,state in config.read()['topics'].items() if state]
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9999)
