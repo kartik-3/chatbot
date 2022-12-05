bdadfd8ca585159b4074a023d048087d08eadf98
Siddhant <gsiddhant159@gmail.com>
2022-12-05 17:05:45 -0500

    added topic labels to comments +indexing script changes

--- data/indexer.py
+++ data/indexer.py
@@ -2,123 +2,86 @@ import os
 import pysolr
 import requests
 import json
+if os.path.exists(dir:='./data'): os.chdir('./data')
 
-CORE_NAME = "chitchat"
-GCP_IP = "34.135.219.198"
+CORE_NAME = "IRF22P4"
+GCP_IP = "34.133.78.26"
+url_solr = f'http://{GCP_IP}:8983/solr/'
+url_core = url_solr+CORE_NAME
+connection = pysolr.Solr(url_core, always_commit=True, timeout=500)
 
 
-def delete_core(core=CORE_NAME):
-    print(os.system('sudo su - solr -c "/opt/solr/bin/solr delete -c {core}"'.format(core=core)))
-
-
-def create_core(core=CORE_NAME):
-    print(os.system(
-        'sudo su - solr -c "/opt/solr/bin/solr create -c {core} -n data_driven_schema_configs"'.format(
-            core=core)))
-
-# collection
-
-# collection = [{
-#     "id": 1,
-#     "first_name": "Chickie",
-#     "last_name": "Proven",
-#     "email": "cproven0@alexa.com",
-#     "age": 77,
-#     "pincodes": [2121212, 3232323]
-# }, {
-#     "id": 2,
-#     "first_name": "Dex",
-#     "last_name": "Bofield",
-#     "email": "dbofield1@about.com",
-#     "age": 88,
-#     "pincodes": [2121212, 3232323]
-# }, {
-#     "id": 3,
-#     "first_name": "Saba",
-#     "last_name": "Ace",
-#     "email": "sace2@craigslist.org",
-#     "age": 55,
-#     "pincodes": [2121212, 3232323]
-# }, {
-#     "id": 4,
-#     "first_name": "Hymie",
-#     "last_name": "Patterfield",
-#     "email": "hpatterfield3@plala.or.jp",
-#     "age": 22,
-#     "pincodes": [2121212, 3232323]
-# }, {
-#     "id": 5,
-#     "first_name": "Chiarra",
-#     "last_name": "Cornils",
-#     "email": "ccornils4@patch.com",
-#     "age": 23,
-#     "pincodes": [2121212, 3232323]
-# }]
-
-with open('chitchat/flat_chitchat_2.json', 'r') as f:
-    data = json.load(f)
-
-class Indexer:
-    def __init__(self):
-        self.solr_url = f'http://{GCP_IP}:8983/solr/'
-        self.connection = pysolr.Solr(self.solr_url + CORE_NAME, always_commit=True, timeout=500)
-
-    def do_initial_setup(self):
-        # delete_core()
-        create_core()
-
-    def create_documents(self, docs):
-        print(self.connection.add(docs))
-
-    def add_fields(self):
-        data = {
+def add_fields():
+    js = {
             "add-field": [
-                # fields for submissions doc
-                {
-                    "name": "body",
-                    "type": "string",
-                    "multiValued": False
-                },
-                {
-                    "name": "id",
-                    "type": "string",
-                    "multiValued": False
-                },
-                {
-                    "name": "created_at",
-                    "type": "pdate",
-                    "multiValued": False
-                },
-                {
-                    "name": "parent_id",
-                    "type": "string",
-                    "multiValued": False
-                },
-                {
-                    "name": "parent_body",
-                    "type": "string",
-                    "multiValued": False
-                },
-                {
-                    "name": "topic",
-                    "type": "string",
-                    "multiValued": False
-                },
-                {
-                    "name": "author",
-                    "type": "string",
-                    "multiValued": False
-                }
+            {
+                "name": "subreddit",
+                "type": "string",
+                "multiValued": False
+            },
+            {
+                "name": "full_link",
+                "type": "string",
+                "multiValued": False
+            },
+            {
+                "name": "author",
+                "type": "string",
+                "multiValued": False
+            },
+            {
+                "name": "is_submission",
+                "type": "boolean",
+                "multiValued": False
+            },
+            {
+                "name": "created_at",
+                "type": "pdate",
+                "multiValued": False
+            },
+            {
+                "name": "topic",
+                "type": "string",
+                "multiValued": False
+            },
+            # submission fields
+            # {
+            #     "name": "title",
+            #     "type": "string",
+            #     "multiValued": False
+            # },
+            # {
+            #     "name": "selftext",
+            #     "type": "text_en",
+            #     "multiValued": False
+            # },
+            #
+            # comment fields
+            {
+                "name": "body",
+                "type": "text_en",
+                "multiValued": False
+            },
+            {
+                "name": "parent_id",
+                "type": "string",
+                "multiValued": False
+            },
+            {
+                "name": "parent_body",
+                "type": "text_en",
+                "multiValued": False
+            },
             ]
         }
-
-        print(requests.post(self.solr_url + CORE_NAME + "/schema", json=data).json())
-
-
-if __name__ == "__main__":
-    i = Indexer()
-    # i.do_initial_setup()    
-    # i.add_fields()
-    # i.replace_fields()
-    i.create_documents(data)
+    print(requests.post(url_core+'/schema', json=js).json())
     
+add_fields()
+
+with open('./chitchat/flat_chitchat_2.json', 'r') as f:
+    chitchat = json.load(f)
+print(connection.add(chitchat))
+
+with open('./redditData/comments-topic-labelled.json', 'r') as f:
+    reddit = json.load(f)
+print(connection.add(reddit))
---
+++ data/redditData/comments/topiclabels.py
@@ -0,0 +1,44 @@
+"""
+To add topic label to comments by searching for the parent submission topic
+"""
+
+import pandas as pd
+import numpy as np
+import os 
+if os.path.exists(dir:="data/redditData"): os.chdir(dir)
+
+comments: pd.DataFrame = pd.read_pickle('./topic_only_submission_comments_df.pkl')
+submissions: pd.DataFrame = pd.read_pickle('./topic_only_submission_df.pkl')
+
+comments = comments.drop_duplicates(subset=['id'])
+comments = comments.sort_values(by=['parent_id','id'])
+comments = comments.reset_index(drop=True)
+comments['topic'] = ''
+
+# comments = comments.head(500)
+
+def label_topic(row:pd.core.series.Series) -> pd.core.series.Series:
+    # print('processing: ',row.id)
+    pid = row.parent_id
+    if str(pid).startswith('t3'):
+        parent = submissions.loc[submissions.id == pid[3:]]
+        if parent.empty: return row
+        row.topic = parent.iloc[0].topic
+    elif str(pid).startswith('t1'):
+        parent = comments.loc[comments.id == pid[3:]]
+        if parent.empty: return row
+        parent = label_topic(parent.iloc[0])
+        if type(parent) is str: return row
+        row.topic = parent.topic
+    else:
+        return row
+    return row
+
+comments = comments.apply(label_topic,axis='columns')
+
+comments.topic = comments.topic.where(comments.topic!='',other='misc')
+comments.topic = comments.topic.str.lower()
+
+comments.to_pickle('comments-topic-labelled.pkl')
+
+print('end')
